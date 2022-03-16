addaa5f7eb57ff7f3aab5eb873cf300318464f40
Pierre GIRAUD <pierre.giraud@dalibo.com>
2022-03-16 08:21:29 +0100

    Init

---
+++ example/src/components/VLink.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { inject } from "vue"
+
+const props = defineProps({
+  to: {
+    type: String,
+    required: true,
+  },
+})
+
+const currentPath = inject("currentPath")
+
+const go = (event) => {
+  event.preventDefault()
+  currentPath.value = props.to
+}
+</script>
+
+<template>
+  <a v-bind:to="to" v-on:click="go" href>
+    <slot></slot>
+  </a>
+</template>
---
+++ src/components/Plan.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+interface Props {
+  planSource: string
+  planQuery: string
+}
+defineProps<Props>()
+</script>
+
+<template>
+  That's me
+  <br />
+  and only me
+  {{ planSource }}
+  {{ planQuery }}
+</template>
