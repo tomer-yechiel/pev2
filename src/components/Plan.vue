b0292180b75de05ae27642c6c838b6435ba8bb19
Pierre GIRAUD <pierre.giraud@dalibo.com>
2022-03-28 21:06:43 +0200

    Import scss in main component
    And reimport splitpanes css

--- src/components/Plan.vue
+++ src/components/Plan.vue
@@ -723,3 +723,9 @@ const triggersTotalDuration = computed(() => {
     </div>
   </div>
 </template>
+
+<style lang="scss">
+@import "../assets/scss/variables";
+@import "../assets/scss/pev2";
+@import "splitpanes/dist/splitpanes.css";
+</style>
--- src/components/PlanNode.vue
+++ src/components/PlanNode.vue
@@ -1101,8 +1101,3 @@ function formattedProp(propName: keyof typeof NodeProp) {
     </ul>
   </div>
 </template>
-
-<style lang="scss">
-@import "../assets/scss/variables";
-@import "../assets/scss/pev2";
-</style>
